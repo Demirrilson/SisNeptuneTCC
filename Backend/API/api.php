6074538a2edad25dec727d199faf9a3a7c791c65
Demirrilson <demirrilson@hotmail.com>
2024-05-15 00:46:48 -0300

    versao1.4.0

--- Backend/API/api.php
+++ Backend/API/api.php
@@ -1,10 +1,8 @@
 <?php
-// Dados do banco de dados MySQL
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "neptune";
-$data_litura = 0000;
 
 // Conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,21 +11,18 @@ if ($conn->connect_error) {
 }
 
 // Receber os dados do ESP32
-$data_litura = $_POST["data_leitura"];
+$sensor_id = $_POST["sensor_id"];
 $valor = $_POST["valor"];
-
-var_dump($_POST);
+$data_leitura = $_POST["data_leitura"];
 
 // Preparar e executar a query SQL
-$stmt = $conn->prepare("INSERT INTO leitura_sensor (Data_leitura, Valor) VALUES (?, ?)");
-$stmt->bind_param("ss", $data_litura, $valor);
+$stmt = $conn->prepare("INSERT INTO leitura_sensor (Sensor_id, Valor, Data_leitura) VALUES (?, ?, ?)");
+$stmt->bind_param("ids", $sensor_id, $valor, $data_leitura);
+
 if ($stmt->execute()) {
     echo "Dados inseridos com sucesso";
-    echo " data $data_litura";
-    echo "valor $valor";
 } else {
     echo "Erro ao inserir dados: " . $conn->error;
-
 }
 
 $stmt->close();
--- Backend/ESP32/leituraSensorTemp/leituraSensorTemp.ino
+++ Backend/ESP32/leituraSensorTemp/leituraSensorTemp.ino
@@ -2,67 +2,100 @@
 #include <WiFi.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <time.h>
+#include <Wire.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+#include <TimeLib.h> // Incluindo a biblioteca TimeLib
 
 // Dados do WiFi
 const char* ssid = "Be2G";
 const char* password = "cenhahu3hu3";
 
-// Configurações do Sensor
-const int oneWireBus = 4;
-OneWire oneWire(oneWireBus);
+// Configurações do Sensor de Temperatura
+const int TEMPERATURE_SENSOR_PIN = 4;
+OneWire oneWire(TEMPERATURE_SENSOR_PIN);
 DallasTemperature sensors(&oneWire);
+
+// Configurações do Sensor Ultrassônico
+const int TRIG_PIN = 26;
+const int ECHO_PIN = 25;
+
+// Configurações do Sensor TDS Meter
+const int TDS_PIN = 27;
 
 // Dados da API PHP
 const char* api_url = "http://192.168.1.21/Neptune/SisNeptuneTCC/Backend/API/api.php";
 
-// Configuração do NTP
-// Configuração do NTP para São Paulo, Brasil
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = -3 * 3600; // GMT offset de 3 horas (em segundos) para São Paulo, Brasil
-const int   daylightOffset_sec = 0;    // São Paulo não está em horário de verão
+// LCD
+hd44780_I2Cexp lcd; // Instanciação do objeto lcd
 
 void setup() {
     Serial.begin(115200);
     sensors.begin();
-    WiFi.begin(ssid, password);
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    pinMode(TDS_PIN, INPUT);
 
+    connectWiFi();
+
+    // Inicialização do LCD
+    lcd.begin(16, 2);
+
+    // Configuração da TimeLib
+    configTime(0, 0, "pool.ntp.org");
+}
+
+void loop() {
+    float temperature = getTemperature();
+    float distance = getDistance();
+    float tdsValue = getTDS();
+
+    sendDataToAPI(1, temperature);
+    sendDataToAPI(2, distance);
+    sendDataToAPI(3, tdsValue);
+
+    displayStatus();
+
+    delay(10 * 1000); // Aguardar 5 minutos para a próxima leitura
+}
+
+void connectWiFi() {
+    WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
     Serial.println("Conectado ao WiFi");
-
-    // Configurar a sincronização de tempo
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-
-    // Esperar pela sincronização de tempo
-    Serial.println("Esperando pela sincronização de tempo...");
-    while (!time(nullptr)) {
-        delay(1000);
-    }
-    Serial.println("Tempo sincronizado");
 }
 
-void loop() {
+float getTemperature() {
     sensors.requestTemperatures();
-    float temperature = sensors.getTempCByIndex(0);
+    return sensors.getTempCByIndex(0);
+}
 
-    // Obter data e hora atual
-    time_t now = time(nullptr);
-    struct tm *timeinfo = localtime(&now);
-    char data_leitura[20];
-    strftime(data_leitura, sizeof(data_leitura), "%Y-%m-%d %H:%M:%S", timeinfo);
+float getDistance() {
+    long duration;
+    float distance;
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = duration * 0.01715;
+    return distance;
+}
 
-    // Enviar dados para a API PHP
+float getTDS() {
+    return analogRead(TDS_PIN);
+}
+
+void sendDataToAPI(int sensor_id, float value) {
     HTTPClient http;
     http.begin(api_url);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    String postData = "data_leitura=" + String(data_leitura) + "&valor=" + String(temperature);
 
-    Serial.print("Enviando dados para a API: ");
-    Serial.println(postData);
-
+    String postData = "sensor_id=" + String(sensor_id) + "&valor=" + String(value) + "&data_leitura=" + getTimeString();
     int httpResponseCode = http.POST(postData);
 
     if (httpResponseCode > 0) {
@@ -77,7 +110,38 @@ void loop() {
     }
 
     http.end();
+}
 
-    delay(10000); // Aguardar 10 segundos para a próxima leitura
+void displayStatus() {
+    if (WiFi.status() == WL_CONNECTED) {
+        lcd.setCursor(0, 0);
+        lcd.print("WiFi: OK");
+    } else {
+        lcd.setCursor(0, 0);
+        lcd.print("WiFi: Erro");
+    }
+
+    HTTPClient http;
+    http.begin(api_url);
+    int httpResponseCode = http.GET();
+    if (httpResponseCode == 200) {
+        lcd.setCursor(0, 1);
+        lcd.print("API: OK");
+    } else {
+        lcd.setCursor(0, 1);
+        lcd.print("API: Erro");
+    }
+
+    http.end();
+}
+
+String getTimeString() {
+    time_t now = time(nullptr);
+    struct tm timeinfo;
+    localtime_r(&now, &timeinfo);
+
+    char timeString[25];
+    strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &timeinfo);
+    return String(timeString);
 }
 
